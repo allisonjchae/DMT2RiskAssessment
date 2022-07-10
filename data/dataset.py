d6bd3682d4a92114a78982cc809757b11d6a05b3
Michael Yao <michael.yao@pennmedicine.upenn.edu>
2022-07-10 17:45:40 -0400

    Pushing local changes

--- data/dataset.py
+++ data/dataset.py
@@ -46,20 +46,35 @@ class PMBBDataset:
         if pkl_file is not None:
             if not isinstance(pkl_file, Path):
                 pkl_file = Path(pkl_file)
-            loaded_data = pickle.load(open(pkl_file, "rb"))
-            self.pmbb_ids = loaded_data["pmbb_ids"]
-            self.data = loaded_data["data"]
-            self.pkl_file = pkl_file
-            return
+            if pkl_file.exists():
+                loaded_data = pickle.load(open(pkl_file, "rb"))
+                self.pmbb_ids = loaded_data["pmbb_ids"]
+                self.data = loaded_data["data"]
+                self.a1c = loaded_data["a1c"]
+                self.t2dm = loaded_data["t2dm"]
+                self.pkl_file = pkl_file
+                if self.verbose:
+                    print(
+                        f"Imported from cache {os.path.abspath(pkl_file)}"
+                    )
+                return
+            elif self.verbose:
+                print(
+                    f"Cache {pkl_file} not found, continuing with import."
+                )
         self.demographics = demographics
         self.phecodes = phecodes
         self.race = race
         self.vitals = vitals
         self.biomarkers = biomarkers
+        if not isinstance(self.biomarkers, Path):
+            self.biomarkers = Path(self.biomarkers)
         self.image1 = image1
         self.image2 = image2
         self.pmbb_ids = set([])
         self.pkl_file = None
+        self.t2dm = {}
+        self.a1c = {}
 
         self.data = self._basic_import()
         self.data.update(self._biomarkers_import())
@@ -76,6 +91,8 @@ class PMBBDataset:
         dataset_as_dict = {}
         dataset_as_dict["pmbb_ids"] = self.pmbb_ids
         dataset_as_dict["data"] = self.data
+        dataset_as_dict["t2dm"] = self.t2dm
+        dataset_as_dict["a1c"] = self.a1c
         pickle.dump(dataset_as_dict, open(file_path, "wb"))
         if self.verbose:
             print(f"Saved dataset to {os.path.abspath(file_path)}")
@@ -104,17 +121,23 @@ class PMBBDataset:
                 print(f"Key {key} not found in dataset. Skipping...")
             return
         key = key.lower()
-        if key not in self.data.keys():
+        if key not in self.data.keys() and key not in ["a1c", "t2dm"]:
             if self.verbose:
                 print(f"Key {key} not found in dataset. Skipping...")
             return
         # Get all the unique IDs from the specified dataset.
         # We assume that all PMBB_IDs in the key dataset have an associated
         # dataset value (ie no "nan" or "None" values).
-        pmbb_id_idx = np.where(self.data[key]["header"] == "PMBB_ID")[0]
+        if key == "a1c":
+            ref_dataset = self.a1c
+        elif key == "t2dm":
+            ref_dataset = self.t2dm
+        else:
+            ref_dataset = self.data[key]
+        pmbb_id_idx = np.where(ref_dataset["header"] == "PMBB_ID")[0]
         assert pmbb_id_idx == 0
         valid_pmbb_ids = set(
-            np.squeeze(self.data[key]["data"][:, pmbb_id_idx]).tolist()
+            np.squeeze(ref_dataset["data"][:, pmbb_id_idx]).tolist()
         )
 
         self.pmbb_ids = self.pmbb_ids.intersection(valid_pmbb_ids)
@@ -127,15 +150,43 @@ class PMBBDataset:
                 datak["data"][:, pmbb_id_idx].shape, dtype=bool
             )
             before_size = np.squeeze(datak["data"][:, pmbb_id_idx]).shape[0]
-            for id in valid_pmbb_ids:
+            for _id in valid_pmbb_ids:
                 valid_idxs[
-                    np.where(datak["data"][:, pmbb_id_idx] == id)
+                    np.where(datak["data"][:, pmbb_id_idx] == _id)
                 ] = True
             datak["data"] = datak["data"][np.squeeze(valid_idxs), ...]
             after_size = np.squeeze(datak["data"][:, pmbb_id_idx]).shape[0]
             self.data[k] = datak
             if self.verbose:
                 print(f"Key {k.upper()}: {after_size} / {before_size} kept.")
+        if key != "a1c":
+            valid_idxs = np.zeros(
+                self.a1c["data"][:, pmbb_id_idx].shape, dtype=bool
+            )
+            before_size = np.squeeze(self.a1c["data"][:, pmbb_id_idx]).shape[0]
+            for _id in valid_pmbb_ids:
+                valid_idxs[
+                    np.where(self.a1c["data"][:, pmbb_id_idx] == _id)
+                ] = True
+            self.a1c["data"] = self.a1c["data"][np.squeeze(valid_idxs), ...]
+            after_size = np.squeeze(self.a1c["data"][:, pmbb_id_idx]).shape[0]
+            if self.verbose:
+                print(f"Key a1c: {after_size} / {before_size} kept.")
+        if key != "t2dm":
+            valid_idxs = np.zeros(
+                self.t2dm["data"][:, pmbb_id_idx].shape, dtype=bool
+            )
+            before_size = np.squeeze(
+                self.t2dm["data"][:, pmbb_id_idx]
+            ).shape[0]
+            for _id in valid_pmbb_ids:
+                valid_idxs[
+                    np.where(self.t2dm["data"][:, pmbb_id_idx] == _id)
+                ] = True
+            self.t2dm["data"] = self.t2dm["data"][np.squeeze(valid_idxs), ...]
+            after_size = np.squeeze(self.t2dm["data"][:, pmbb_id_idx]).shape[0]
+            if self.verbose:
+                print(f"Key a1c: {after_size} / {before_size} kept.")
 
         if self.verbose:
             print()
@@ -337,6 +388,15 @@ class PMBBDataset:
             # Skip any particular files.
             if "fasting_glucose" in fn.stem.lower():
                 continue
+            # Skip CRP since PMBB IDs are not available in CRP dataset.
+            elif "crp" in fn.stem.lower() or "urobili" in fn.stem.lower():
+                continue
+            # Skip Urobili since PMBB IDs are not available in Urobili dataset.
+            elif "urobili" in fn.stem.lower():
+                continue
+            # Skip ALB since PMBB IDs are not available in ALB dataset.
+            elif "alb" in fn.stem.lower():
+                continue
             elif not fn.as_posix().lower().endswith(".csv"):
                 continue
 
@@ -347,6 +407,7 @@ class PMBBDataset:
                     print("Skipping " + fn.stem, flush=True)
                 continue
             f = pd.read_csv(fn)
+            f.dropna(inplace=True)
             if "bmi" in fn.stem.lower():
                 f.drop(["PATIENT_CLASS"], axis=1, inplace=True)
             elif "dbp" in fn.stem.lower():
@@ -365,9 +426,16 @@ class PMBBDataset:
                     "PATIENT_CLASS", "ORDER_NAME", "ABNORMAL"
                 ], axis=1, inplace=True)
             elif "t2dm_status" in fn.stem.lower():
-                # Drop T2DM status data.
+                f.drop(["PACKET_UUID"], axis=1, inplace=True)
+                f.replace("CONTROL", 0, inplace=True)
+                f.replace("INSUFFICENT_EVIDENCE", 0, inplace=True)
+                f.replace("CASE", 1, inplace=True)
+                self.t2dm = {
+                    "header": np.array(list(f.columns)),
+                    "data": f.to_numpy()
+                }
                 if self.verbose:
-                    print("Skipping " + fn.stem, flush=True)
+                    print("Found ground truth T2DM statuses.")
                 continue
             elif "antihypertensive" in fn.stem.lower():
                 f.drop([
@@ -409,6 +477,8 @@ class PMBBDataset:
                     "RESULT_DATE_SHIFT",
                     "ABNORMAL",
                 ], axis=1, inplace=True)
+                if "PATIENT_CLASS" in f.columns:
+                    f.drop(["PATIENT_CLASS"], axis=1, inplace=True)
             if self.verbose:
                 print("Imported " + fn.stem, flush=True)
 
@@ -420,6 +490,14 @@ class PMBBDataset:
                     "header": np.array(list(f.columns))[:2],
                     "data": f.to_numpy()[:, :2]
                 }
+            elif "a1c" in fn.stem.lower():
+                self.a1c = {
+                    "header": np.array(list(f.columns)),
+                    "data": f.to_numpy()
+                }
+                if self.verbose:
+                    print("Found ground truth A1C measurements.")
+                continue
             else:
                 data[key] = {
                     "header": np.array(list(f.columns)),
